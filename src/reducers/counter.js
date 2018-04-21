9ef0426dcc0e567fac3f340437e4c2d549c688c1
elementsweb <elementsweb@users.noreply.github.com>
2018-04-21 16:23:09 +0100

    Fix indentation of switch statement

--- src/reducers/counter.js
+++ src/reducers/counter.js
@@ -10,7 +10,7 @@ const update = (namespace = 'default') => (state = initialState, action) => {
     case `@@component/${namespace}/${INCREMENT_COUNTER}`:
       return state + 1;
 
-      case `@@component/${namespace}/${DECREMENT_COUNTER}`:
+    case `@@component/${namespace}/${DECREMENT_COUNTER}`:
       return state - 1;
 
     default:
